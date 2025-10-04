b3e53fb2f54de5ab235345c2e341bcaf015b2ca4
hsuliz <hlib.suliz@gmail.com>
2025-10-04 19:27:34 +0200

    refactor

--- src/test/java/com/github/hsuliz/aiagentsample/domain/OrchestratorIT.java
+++ src/test/java/com/github/hsuliz/aiagentsample/domain/OrchestratorIT.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class OrchestratorIntegrationTest {
+public class OrchestratorIT {
 
   @Autowired Orchestrator orchestrator;
 
